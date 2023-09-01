183ed378c856a11961180d045f57a1abe1a22e53
rokudou35 <143547720+rokudou35@users.noreply.github.com>
2023-09-01 07:42:35 +0200

    Create HelloWorld.sol

---
+++ HelloWorld.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BSD-2-Clause
+pragma solidity ^0.8.16;
+
+contract HelloWorld {
+  // State Variables
+  string greeting = ""; // Change this
+  
+  // Endpoints
+  // ...
+}
