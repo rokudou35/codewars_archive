a70e3f4b9fd5ecf495d13f9b0f954fbfa293e413
rokudou35 <143547720+rokudou35@users.noreply.github.com>
2023-09-03 17:44:38 +0200

    Create angles.sol

---
+++ angles.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: BSD-2-Clause
+pragma solidity ^0.8.16;
+
+contract Kata {
+  // Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+  function angle(int n) public pure returns (int) {
+    // your code here
+  }
+}
